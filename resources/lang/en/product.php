644a2770037aeb1562aae0e4cd706c1ee84f7e82
JSDev00 <73703231+JSDev00@users.noreply.github.com>
2024-04-20 02:17:34 +0200

    .
    .

---
+++ resources/lang/en/product.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Name'=>'Product Name',
+    'Desc'=>'Product Description',
+    'Price'=>'Price',
+    'Quantity'=>'Quantity',
+    'Category'=>'Category',
+    'Actions'=>'Actions'
+];
